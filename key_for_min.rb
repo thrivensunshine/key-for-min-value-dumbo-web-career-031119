e3b8050cde33a51968f6882f47987ba6d82a1ab5
DANIELLE ALBA <danib.sgh@gmail.com>
2019-02-24 21:59:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,13 +9,13 @@ end
 
 
 hash.collect do |key, value|
-test << value
-keys << key
+  test << value
+  keys << key
 end
-if test[0]< test[1] && test[0] < test[2]
-keys[0]
-elsif test[1]< test[0] && test[1] < test[2]
-keys[1]
-else keys[2]
-end
+  if test[0]< test[1] && test[0] < test[2]
+    keys[0]
+  elsif test[1]< test[0] && test[1] < test[2]
+    keys[1]
+  else keys[2]
+  end
 end
